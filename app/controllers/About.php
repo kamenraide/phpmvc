63747a6eb796b142b6e2b68dc07ad26983b902af
Kamenraide <kamenraiderpowerranger47@gmail.com>
2022-06-22 21:46:56 +0700

    menambahkan file task 2 video 1-4

---
+++ app/controllers/About.php
@@ -0,0 +1,12 @@
+<?php
+
+class About {
+	public function index($nama = 'sandhika', $pekerjaan = 'dosen')
+	{
+		echi "Halo, nama saya $nama, saya adalah seorang $pekerjaan";
+	}
+	public funtion page()
+	{
+		echo 'About/page';
+	}
+}
